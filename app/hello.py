acaa9a39713e85731ab8c30b753d7cae1b149692
Chris Swanson <chris.swanson6@gmail.com>
2021-12-06 20:45:04 -0800

    added fly build for flask hello

---
+++ app/hello.py
@@ -0,0 +1,12 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name="World"):
+    return f'Hello, {name}!'
+
+@app.route('/health')
+def health():
+    return Response("👍", status=418)
